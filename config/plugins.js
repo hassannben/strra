e0717bf3878ee5dfe8af00c19e5f42be4546492b
hassannben <fmomen977@gmail.com>
2024-08-19 23:05:42 +0100

    config/server.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,28 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    // Example plugin configuration
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'yourDefaultJwtSecret'),
+      },
+    },
+    upload: {
+      config: {
+        provider: 'local',
+        providerOptions: {
+          sizeLimit: 1000000, // Set file size limit
+        },
+      },
+    },
+    email: {
+      config: {
+        provider: 'sendmail',
+        providerOptions: {
+          secure: true,
+          defaultFrom: env('EMAIL_DEFAULT_FROM', 'fmomen977@gmail.com'),
+          defaultReplyTo: env('EMAIL_DEFAULT_REPLY_TO', 'fmomen977@gmail.com'),
+        },
+      },
+    },
+    // Add more plugins as needed
+  });
+  
--- config/server.js
+++ config/server.js
@@ -1,10 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'), // Sets the host, defaulting to '0.0.0.0' which binds to all network interfaces
+  port: env.int('PORT', 1337), // Sets the port, defaulting to 1337
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS'), // Secret keys used by the application, loaded from environment variables
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false), // Configures whether webhook payloads should populate relations
   },
 });
